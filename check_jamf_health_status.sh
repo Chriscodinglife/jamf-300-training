1acb0d846f005b20007f609d73069ae7ebead7ee
Chriscodinglife <chrisdotorellana@gmail.com>
2022-08-08 13:31:19 -0400

    Created the health_check jamf server script for class

---
+++ check_jamf_health_status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Using a curl command to find the health status of my JAMF Pro Server
+# Also use a -s with curl to hide the header information
+jamf_health_url="https://m300-10a.pro.jamf.training/healthCheck.html"
+health_check=$(curl -s $jamf_health_url)
+
+# Using echo to sanity check my command substitution
+# echo $health_check
+
+if [[ $health_check == "[]" ]]; then
+    echo "JAMF Pro Server is healthy"
+else
+    echo "JAMF Pro Server is not healthy"
+fi
+
+
