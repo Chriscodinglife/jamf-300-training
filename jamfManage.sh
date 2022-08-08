62929e6e83d517c06cc0f3e7d2e9e49867e45d1c
Chriscodinglife <chrisdotorellana@gmail.com>
2022-08-08 15:29:22 -0400

    Renamed the jamfManage file

--- jamfManage.sh
+++ jamfManage.sh
@@ -2,7 +2,6 @@
 
 # This command will update the management framework on the loval device
 sudo jamf manage
-
 # Use the date command to time stamp when the script ran
 management_text_file="/Users/Shared/ManangementTime.txt"
 timestamp=$(date +%F\ %T)
