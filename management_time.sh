619c5a55834d1f454deebc5098d44d06ee766c75
Chriscodinglife <chrisdotorellana@gmail.com>
2022-08-08 15:16:58 -0400

    Created the basic structure for the management_time.sh script

---
+++ management_time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This command will update the management framework on the loval device
+sudo jamf manage
+
+# Use the date command to time stamp when the script ran
+management_text_file="/Users/Shared/ManangementTime.txt"
+timestamp=$(date +%F\ %T)
+echo $timestamp >> $management_text_file
