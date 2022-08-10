eb7fd379a50709a4e1ee980a441007176d3461b0
Chriscodinglife <chrisdotorellana@gmail.com>
2022-08-10 17:01:55 -0400

    updated the managementtime typo. woops.

--- scripts/lesson_10/run_jamfmanage_post_timestamp.sh
+++ scripts/lesson_10/run_jamfmanage_post_timestamp.sh
@@ -3,6 +3,6 @@
 # This command will update the management framework on the local device
 sudo jamf manage
 # Use the date command to time stamp when the script ran
-management_text_file="/Users/Shared/ManangementTime.txt"
+management_text_file="/Users/Shared/managementTime.txt"
 timestamp=$(date +%F\ %T)
 echo $timestamp >> $management_text_file
